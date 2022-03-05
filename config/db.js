a83a4690c8ed3c8a9a90843873931b102c9e2ea4
varun <varunrana829@gmail.com>
2022-03-05 21:41:13 +0530

    new changes

--- config/db.js
+++ config/db.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const db = await mongoose.connect(process.env.MONGO_URI);
+    const db = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
     console.log(`mongoDB connected: ${db.connection.host}`);
   } catch (error) {
